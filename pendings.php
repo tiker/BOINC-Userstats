f9c34c635bffcb4b4a07ed435f9eae5652f456e4
XSmeagolX <XSmeagolX@gmx.net>
2017-09-30 13:54:27 +0200

    pendings.php auf php7/mysqli

--- pendings.php
+++ pendings.php
@@ -6,19 +6,19 @@ date_default_timezone_set('UTC');
 //-----------------------------------------------------------------------------------
 
 // Sprachdefinierung
-if(isset($_GET["lang"])) $lang=$_GET["lang"];
+if (isset($_GET["lang"])) $lang = $_GET["lang"];
 else $lang = strtolower(substr($_SERVER['HTTP_ACCEPT_LANGUAGE'],0,2));
 
 ############################################################
 # Beginn fuer Datenzusammenstellung User
-$result_user=mysql_query("SELECT * from boinc_user");  //alle Userdaten einlesen
-	while($row=mysql_fetch_assoc($result_user)){
-		$project_username = $row["boinc_name"];
-		$project_wcgname = $row["wcg_name"];
-		$project_teamname = $row["team_name"];
-		$cpid = $row["cpid"];
-		$datum_start = $row["lastupdate_start"];
-		$datum = $row["lastupdate"];
+$result_user = mysqli_query($db_conn, "SELECT * FROM boinc_user");  //alle Userdaten einlesen
+while ($row = mysqli_fetch_assoc($result_user)) {
+	$project_username = $row["boinc_name"];
+	$project_wcgname = $row["wcg_name"];
+	$project_teamname = $row["team_name"];
+	$cpid = $row["cpid"];
+	$datum_start = $row["lastupdate_start"];
+	$datum = $row["lastupdate"];
 	}
 #	$datum_start=$datum_start + 3600;
 	$lastupdate_start = date("d.m.Y H:i:s",$datum_start);
@@ -61,73 +61,73 @@ else include "./lang/en.txt.php";
 
 
 <div class="section text-center section-default">
-	<div class="container">
-		<div class="main">
-			<div class="section section-standard">
-				<div class="container">
-					<h2 class="title-uppercase text-center">Pending Credits</h2>
-					<div class="alert alert-success">
-        		    		<div class="container">
-						<? echo $tr_hp_pendings_02; ?>		
+    <div class="container">
+        <div class="main">
+            <div class="section section-standard">
+                <div class="container">
+                    <h2 class="title-uppercase text-center">Pending Credits</h2>
+                    <div class="alert alert-success">
+                        <div class="container">
+                            <?php echo $tr_hp_pendings_02; ?>
 
-					</div>
-					</div>
-<table  class="table table-striped table-hover text-right table-condensed" style="background: linear-gradient(to bottom, #FFFFFF 70%, #F3F3F3 100%); box-shadow: 0 1px 2px rgba(0,0,0,0.4);">
+                        </div>
+                    </div>
+                    <table class="table table-striped table-hover text-right table-condensed"
+                           style="background: linear-gradient(to bottom, #FFFFFF 70%, #F3F3F3 100%); box-shadow: 0 1px 2px rgba(0,0,0,0.4);">
 
-			<thead>
-				<tr>
-					<th class="text-right"><?php echo $tr_tb_pr ?></th>
-					<th class="text-left"><?php echo $tr_tb_pe ?></th>
-				</tr>
-			</thead>
-			<tbody>
+                        <thead>
+                        <tr>
+                            <th class="text-right"><?php echo $tr_tb_pr ?></th>
+                            <th class="text-left"><?php echo $tr_tb_pe ?></th>
+                        </tr>
+                        </thead>
+                        <tbody>
 
-<?
-#------------------------------------------------------
-# Abrufen der persönlichen Stats-Daten
-#------------------------------------------------------
-$query=mysql_query("SELECT * from boinc_grundwerte WHERE project_status ='1'") or die (mysql_error);  //nur bei aktiven Projekten Werte lesen
-#echo "Fehler: " .mysql_error();
-$ctx = stream_context_create(array(
-  'http' => array(
-      'timeout' => 1
-      )
-  )
-);
-$xml_string_pendings = "false";
-while($row=mysql_fetch_assoc($query))
-	{
+                        <?php
+                        #------------------------------------------------------
+                        # Abrufen der persönlichen Stats-Daten
+                        #------------------------------------------------------
+                        $query = mysqli_query($db_conn, "SELECT * FROM boinc_grundwerte WHERE project_status ='1'") or die (mysqli_error);  //nur bei aktiven Projekten Werte lesen
+                        #echo "Fehler: " .mysqli_error();
+                        $ctx = stream_context_create(array(
+                                'http' => array(
+                                    'timeout' => 1
+                                )
+                            )
+                        );
+                        $xml_string_pendings = "false";
+                        $pendings_gesamt = null;
+                        while ($row = mysqli_fetch_assoc($query)) {
 #------------ Abrufen der WebRPC-Daten ------------------------
-  $xml_string_pendings = @file_get_contents ($row['url'] . "pending.php?format=xml&authenticator=" . $row['authenticator'], 0, $ctx);
-		if ($xml_string_pendings == FALSE) {
-			$projectname = $row['project'];
-			$pending_credits = $row['pending_credits'];
-		} 
-		else {
-				$projectname = $row['project'];
-			   $xml_pendings = @simplexml_load_string($xml_string_pendings);
-			   $pending_credits = intval($xml_pendings->total_claimed_credit);
-		}
-		$pendings_gesamt= $pendings_gesamt + $pending_credits;
-  	   $sql_pendings  = "UPDATE boinc_grundwerte SET pending_credits='" .$pending_credits. "' WHERE project_shortname='" .$row['project_shortname']. "'";   //aktuelle Pendings des Projektes in Grundwerttabelle eintragen
-	   mysql_query($sql_pendings);  //Werte in DB eintragen
+                            $xml_string_pendings = @file_get_contents($row['url'] . "pending.php?format=xml&authenticator=" . $row['authenticator'], 0, $ctx);
+                            if ($xml_string_pendings == FALSE) {
+                                $projectname = $row['project'];
+                                $pending_credits = $row['pending_credits'];
+                            } else {
+                                $projectname = $row['project'];
+                                $xml_pendings = @simplexml_load_string($xml_string_pendings);
+                                $pending_credits = intval($xml_pendings->total_claimed_credit);
+                            }
+                            $pendings_gesamt = $pendings_gesamt + $pending_credits;
+                            $sql_pendings = "UPDATE boinc_grundwerte SET pending_credits='" . $pending_credits . "' WHERE project_shortname='" . $row['project_shortname'] . "'";   //aktuelle Pendings des Projektes in Grundwerttabelle eintragen
+                            mysqli_query($db_conn, $sql_pendings);  //Werte in DB eintragen
 #	   echo $projectname .": ". $pending_credits ."<br>";
-	   
-				echo "  <tr><td class='text-right'>" .$projectname. "</td>";
-				echo "  <td class='text-left'>" .number_format($pending_credits,0,$dec_point,$thousands_sep). "</td></tr>";
-}
-				echo "  <tr><td class='text-right'>GESAMT Pendings</td>";
-				echo "  <td class='text-left'>" .number_format($pendings_gesamt,0,$dec_point,$thousands_sep). "</td></tr>";
-?>
-</tbody>
-</table>
 
-  				</div>
-			</div>
-		</div>	    
-	</div>
+                            echo "  <tr><td class='text-right'>" . $projectname . "</td>";
+                            echo "  <td class='text-left'>" . number_format($pending_credits, 0, $dec_point, $thousands_sep) . "</td></tr>";
+                        }
+                        echo "  <tr><td class='text-right'>GESAMT Pendings</td>";
+                        echo "  <td class='text-left'>" . number_format($pendings_gesamt, 0, $dec_point, $thousands_sep) . "</td></tr>";
+                        ?>
+                        </tbody>
+                    </table>
+
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 
 <?php echo "$tr_hp_footer" ?>
-	</body>
+</body>
 </html>
