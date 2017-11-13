e3be5a03adc6cb420e09d18edf1e25f509eaf0c7
XSmeagolX <XSmeagolX@gmx.net>
2017-11-13 12:08:21 +0100

    Version dev_22

--- web/footer.php
+++ web/footer.php
@@ -3,15 +3,16 @@
 				<div class="container text-light">
 					<nav>
 						<div class="copyright pull-right text-center">
-							<a href="https://userstats.timo-schneider.de" target="_blank">BOINC Userstats</a> <?php echo $userstats_version; ?>, <i class="fa fa-copyright fa-lg"></i> 2017 by <a href="https://timo-schneider.de" target="_blank">Timo Schneider</a><br><font size="1">(XSmeagolX - Team <a href="https://seti-germany.de"><img src="./images/sg_logo_klein.png"></a>)</font>
+							<a href="https://userstats.timo-schneider.de" target="_blank">BOINC Userstats</a> 
+							<?php echo $userstats_version; ?><br>made with <font color="#f57c7c"><i class="fa fa-heart"></i></font> by <a href="https://timo-schneider.de" target="_blank">Timo Schneider</a><br><font size="1">(XSmeagolX - Team <a href="https://seti-germany.de"><img src="./images/sg_logo_klein.png"></a>)</font>
 							<br>
 						</div>
 						<div class="copyright pull-left text-center">
+						Diese Seite verwendet <a href="https://www.highcharts.com"><i class="fa fa-copyright"></i> Highcharts</a><br>
 							<a href="#" data-toggle="modal" data-target="#modalImpressum">Impressum</a> | 
 							<a href="#" data-toggle="modal" data-target="#modalDisclaimer">Disclaimer</a> | 
 							<a href="#" data-toggle="modal" data-target="#modalDatenschutz">Datenschutz</a> | 
 							<a href="https://github.com/XSmeagolX/Pers-nliche-BOINC-Userstats/blob/master/LICENSE" target="_new">MIT License</a> 
-							<br><font size="1">Diese Seite verwendet <a href="https://www.highcharts.com">Highcharts<i class="fa fa-copyright"></i></a></font>
 						</div>
 					</nav>
 				</div>
