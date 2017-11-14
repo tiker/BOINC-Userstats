e34191a15acf758c835a9155b883c107b718afd9
XSmeagolX <XSmeagolX@gmx.net>
2017-11-14 13:41:00 +0100

    Version dev_26

--- web/footer.php
+++ web/footer.php
@@ -10,7 +10,7 @@
 							</div>
 							<div class="col-12 col-sm-6 col-md-5 text-center align-self-center">
 								<a href="https://userstats.timo-schneider.de" target="_blank">BOINC Userstats</a> 
-								<?php echo $userstats_version; ?><br>made with <font color="#f57c7c"><i class="fa fa-heart"></i></font> by <a href="https://timo-schneider.de" target="_blank">Timo Schneider</a><br><font size="1">(XSmeagolX - Team <a href="https://seti-germany.de"><img src="./assets/images/sg_logo_klein.png"></a>)</font>
+								<?php echo $userstats_version; ?><br>made with <font color="#f57c7c"><i class="fa fa-heart"></i></font> by <a href="https://timo-schneider.de" target="_blank">Timo Schneider</a><br><font size="1">XSmeagolX - Team <a href="https://seti-germany.de"><img src="./assets/images/sg_logo_klein.png"></a></font>
 							</div>
 						</div>
 				</div>
