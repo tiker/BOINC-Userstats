75c0cf945e3146a2af879150ebfbf2d2a7af291f
XSmeagolX <XSmeagolX@gmx.net>
2017-11-24 16:19:27 +0100

    Version dev_32

--- web/footer.php
+++ web/footer.php
@@ -1,14 +1,14 @@
 			<footer class="footer text-sm bg-dark">
-				<div class="container text-light">
+				<div class="text-light">
 						<div class="row">
-							<div class="col-12 col-sm-6 col-md-7 text-center align-self-center">
+							<div class="col-12 col-md-6 mx-auto text-center align-self-center">
 							Diese Seite verwendet <a href="https://www.highcharts.com"><i class="fa fa-copyright"></i> Highcharts</a><br>
 								<a href="#" data-toggle="modal" data-target="#modalImpressum">Impressum</a> | 
 								<a href="#" data-toggle="modal" data-target="#modalDisclaimer">Disclaimer</a> | 
-								<a href="#" data-toggle="modal" data-target="#modalDatenschutz">Datenschutz</a><br>
+								<a href="#" data-toggle="modal" data-target="#modalDatenschutz">Datenschutz</a> | 
 								<a href="https://github.com/XSmeagolX/Pers-nliche-BOINC-Userstats/blob/master/LICENSE" target="_new">MIT License</a> 
 							</div>
-							<div class="col-12 col-sm-6 col-md-5 text-center align-self-center">
+							<div class="col-12 col-md-6 mx-auto text-center align-self-center order-first">
 								<a href="https://userstats.timo-schneider.de" target="_blank">BOINC Userstats</a> 
 								<?php echo $userstats_version; ?><br>made with <font color="#f57c7c"><i class="fa fa-heart"></i></font> by <a href="https://timo-schneider.de" target="_blank">Timo Schneider</a><br><font size="1">XSmeagolX - Team <a href="https://seti-germany.de"><img src="./assets/images/sg_logo_klein.png"></a></font>
 							</div>
