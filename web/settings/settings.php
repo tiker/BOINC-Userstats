088c8069f376543acd29c2d3335f7812a9dbe2c6
XSmeagolX <XSmeagolX@gmx.net>
2018-03-02 15:06:30 +0100

    prepare to next release

--- web/settings/settings.php
+++ web/settings/settings.php
@@ -1,10 +1,11 @@
 <?php
 	// -----------------------------------------------------------------------------------
-	// In dem folgenden Block bitte die Variablen mit Werten 
-	// entsprechend der Anmerkung vornehmen
+	// In dem folgenden Block bitte die Variablen mit Werten entsprechend der Anmerkung vornehmen
+	// Change the values of the following section as descriped
 	// -----------------------------------------------------------------------------------
 	
 	// Anzeigen von Fehlermeldungen
+	// Show Error-Messages
 	error_reporting(E_ALL);
 	ini_set ('display_errors', 'Off'); // für Testumgebung auf 'On' setzen - set to 'On' for test environment
 
@@ -18,8 +19,8 @@
 	// No support for Daylight Savings at the moment.
 	$timezone_shortname = "(MEZ)"; 
 
-	// Diese Variable bitte nicht ändern, ansosten werden deine Werte falsch berechnet!
-	// Do not change the this option, otherwise your data will be calculated wrong!
+	// Diese Variable bitte NICHT ändern, ansosten werden deine Werte falsch berechnet!
+	// Do NOT change the this option, otherwise your data will be calculated wrong!
 	$useUTCHighchartsOption = "true"; // default = "true"
 
 	// Wenn dein Webserver die Zeit in der deiner Zeitzone anzeigt, so lasse hier die "0" stehen
@@ -30,7 +31,7 @@
 
 	// Version einbinden   
 	// Include version
-	include "version.php";
+	include "version.php"; // Den Inhalt dieser Datei nicht verändern!    // Do not change the content of this file
 
 	// Pfad zur Datenbank-Verbindung     
 	// Path to database connect
@@ -47,12 +48,12 @@
 
 	// Konfiguration des Brand-Logos in der Navbar
 	// Configure the brand logo in Navbar
-	$brand_logo = "./assets/images/brand_logo.png"; //das kleine Logo oben links neben dem Namen in der Navbar
-	$hp_nav_brand_link = "./index.php"; //wohin der Link auf dem Brand-Logo führen soll
+	$brand_logo = "./assets/images/brand_logo.png"; //das kleine Logo oben links neben dem Namen in der Navbar  // link to the image-file for the brand-logo top left
+	$hp_nav_brand_link = "./index.php"; //wohin der Link auf dem Brand-Logo führen soll   // the target of the link on the brand logo
 
 	// individuelle Links
 	// Individual Links
-	$showLinks = false; // true - wenn die folgenden Links in der Navbar angezeigt werden sollen
+	$showLinks = false; // true - wenn die folgenden Links in der Navbar angezeigt werden sollen  // if "true", those links and names will be shown in the navbar addtionally
 	$hp_nav_name01 = "Link-Name#1";
 	$hp_nav_link01 = "http://link.zu.Link-Name#1";
 	$hp_nav_name02 = "Link-Name#2";
@@ -72,12 +73,16 @@
 	// User BOINC-Badges
 	// Die Badge-Grafik kann man auch per cronjob 1x täglich in sein Webverzeichnis kopieren und dann hier relativ verlinken.
 	// Dafür ist die Einrichtung der Badges über die Registrierung auf der Homepage von http://signature.statseb.fr notwendig.
+	// You can fetch this signature by a daylie cronjob as well and link them as a relative link
+	// You need to register and configure a signature on http://signature.statsseb.fr
 	$showUserBadges = false; // true - wenn angezeigt werden soll, ansonsten auf false setzen. Standard ist false
 	$linkUserBadges = "https://signature.statseb.fr/sig-12.png"; //Hier den Link zu Deinem User-Badge von http://signature.statseb.fr einfuegen
 	
 	// WCG-Badge-Signatur
 	// Die WCG-Signatur kann man auch per cronjob 1x täglich in sein Webverzeichnis kopieren und dann hier relativ verlinken.
 	// Dafür ist die Einrichtung der WCG-Signatur auf der Homepage von wcgsig.com notwendig (oben in der Mitte - Request a signature).
+	// You can fetch this signature by a daylie cronjob as well an link them as a relative link
+	// You need to have a configure your WCG-Signature on wcgsig.com
 	$showWcgLogo = false; // true - wenn angezeigt werden soll, ansonsten auf false setzen. Standard ist false
 	$linkWcgSig = "http://wcgsig.com/653215.gif"; //Hier den Link zu deinem WCG-Logo von http://wcgsig.com einfuegen
 	
@@ -85,10 +90,13 @@
 	// Die Badge-Grafik kann man auch per cronjob 1x täglich in sein Webverzeichnis kopieren und dann hier relativ verlinken.
 	// Hier kannst du die WCG-Badges des Badge-Generators von SETI.Germany integrieren. 
 	// Hinter badge_ folgt dein WCG-Name, danach _ gefolgt von der Anzahl an Reihen. 0 steht für alle Badges in einer Reihe (halte ich am sinnvollsten)
+	// You can fetch this signature by a daylie cronjog as well and link them as a relative link
+	// to configure this signature change your WCG-Name (XSmeagolX) and the number of rows (0) that should be used
 	$showSgWcgBadges = false; // true - wenn angezeigt werden soll, ansonsten auf false setzen. Standard ist false
 	$linkSgWcgBadges = "https://www.seti-germany.de/wcg/badge_XSmeagolX_0.png"; // WCG-Badges von XSmeagolX, alle in einer Reihe
 	
 	// Hier dein Teamname und die URL zu der Homepage deines Teams eintragen
+	// Your Teamname, Homepage of your team
 	// Falls die Links zum Dev nicht angezeigt werden sollen, oder man ist Mitglied eines anderen Teams,
 	// so kann hier individuell der Link zum BOINC-Team aktiviert werden
 	$hasTeamHp = false; // true - wenn angezeigt werden soll, ansonten auf false setzen. Standard ist false
@@ -96,16 +104,21 @@
 	$teamHpURL = "https://www.seti-germany.de";
 	
 	// Hier die URL zu deinen Statistiken bei boincstats.com
+	// Link to your stats on boincstats.com. Set to true will activate a link in the nav-bar
 	$hasBoincstats = false; // true - wenn angezeigt werden soll, ansonten auf false setzen. Standard ist false
 	$linkNameBoincstats = "BOINCStats";
 	$linkBoincstats = "https://boincstats.com/de/stats/-5/user/detail/15232873522/projectList"; //Link zu boincstats.com von XSmeagolX
 	
 	// Hier die URL zu SG-Stats von XSmeagolX aktivieren
+	// set to "true" will show a link to team statistics of Team SETI.Germany
 	$hasSGStats = true; // true - wenn angezeigt werden soll, ansonten auf false setzen. Standard ist true
 	$linkNameSGStats = "SG-Stats";
 	$linkSGStats = "https://sg.boinc-userstats.de/teamstats/status_index.php"; //Link zu den SG-Stats von XSmeagolX
 
 	// Hier die URL zu den laufenden WUs, falls Du diese mit Boinctasks veröffentlichst
+	// wenn ja, nutze bitte meine boinctasks-Vorlage
+	// if you use BoincTasks and publish your tasks to the internet, set the link to the tasks.html-file on the web
+	// be aware to use my boinctasks-template
 	$hasBoinctasks = false; // true - wenn angezeigt werden soll, ansonten auf false setzen. Standard ist false
 	$linkNameBoinctasks = "Tasks";
 	$linkBoinctasks = "./tasks.php"; // Link zu den laufenden WUs von XSmeagolX
@@ -121,11 +134,13 @@
 
 	// Hier die URL zum WCG, wird im Seitenkopf angezeigt
 	// Falls die "show more"-Links nicht angezeigt werden sollen, so kann hier individuell der Link zum WCG aktiviert werden
+	// Invite-Link to World Community Grid
 	$hasWcg = false; // true - wenn angezeigt werden soll, ansonten auf false setzen. Standard ist false
 	$linkNameWcg = "WCG";
 	$linkWcg = "https://join.worldcommunitygrid.org/?recruiterId=653215&teamId=4VVG5BDPP1";
 
 	// Option zum Anzeigen eines Links zum Aktualisieren der Pending Credits
+	// Option to show Link to refresh pending credits
 	$hasPendings = true; // true - wenn angezeigt werden soll, ansonten auf false setzen. Standard ist false
 	$linkNamePendings = "Pendings";
 	$linkPendings = "./pendings.php"; // Link zu den laufenden WUs von XSmeagolX
@@ -133,10 +148,16 @@
 	// Update-Check
 	// Option zur automatischen Prüfung, ob ein Update verfügbar ist und dies in der Navbar angezeigt wird.
 	// Option to automatically check for updates and show in Navbar if update is available
+	// Wenn diese Option auf "true" gesetzt wird, so werden GET-Anfragen auf die Datei latest_release.xml auf meinem Webservers generiert, um die aktuelle Versionsnummer zu erhalten.
+	// Diese Anfragen werden in meinem Apache-Log gespeichert. Eine Auswertung, Verarbeitung oder Weitergabe dieser Daten findet nicht statt.
+	// If this option is set to "true", your server will generate GET-requests to my Webserver to the file latest_release.xml to get the latest version number.
+	// Those will be stored in my apache web logfiles but those will not be analysed or published.
 	$setUpdatecheck = false; // true - wenn angezeigt werden soll, ansonten auf false setzen. Standard ist true
 	
 	// Auswahl der Projekte fuer Tortendiagramm
+	// Option for Pie Chart
 	// Hier den Wert eintragen, ab welchem Prozentanteil die Projekte separat im Kuchen ausgegeben werden sollen (Standard 1 fuer ab 1%, kann aber auch 10, 2, 0.3 eingetragen werden)
+	// set to a value (%), where projects should be shown individually in the pie chart. All project less than this value are "other". 
 	// 1 für Projekte ab 1% Gesamtanteil
 	// 0 für alle Projekte
 	$separat = 0.9;
